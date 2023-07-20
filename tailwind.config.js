3bb0c91c41daff9191f47a1a97a227a599dbcf3b
Brett Adams <brett@sepal.app>
2023-07-20 09:32:01 -0400

    contact form, tailwind, footer

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: ["forms", "typography", "aspect-ratio"]
+}
